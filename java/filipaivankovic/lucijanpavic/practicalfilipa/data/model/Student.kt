657c4a34241b962503695023b074cb7e5cb7f8a4
FIvankovic <60714787+FIvankovic@users.noreply.github.com>
2023-10-17 14:54:22 +0200

    Delete java/filipaivankovic/lucijanpavic/practicalfilipa directory

--- java/filipaivankovic/lucijanpavic/practicalfilipa/data/model/Student.kt
+++
@@ -1,9 +0,0 @@
-package filipaivankovic.lucijanpavic.practicalfilipa.data.model
-
-
-import com.google.gson.annotations.SerializedName
-
-data class Student(
-    @SerializedName("students")
-    val students: List<StudentX>
-)
